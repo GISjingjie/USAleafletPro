c96da9bc12415fbd98b7de205dfe92c525301a67
someone159 <937620971@qq.com>
2022-10-20 19:06:26 +0800

    spider data to the project

--- spiderUSACovidCases/spider1.js
+++ spiderUSACovidCases/spider1.js
@@ -4,10 +4,12 @@
 const https=require('https');
 //引入cheerio
 const  cheerio=require('cheerio');
+const fs=require('fs')
 //const { find } = require('cheerio/lib/api/traversing');
 let allData=[];
+let data51=[];
 //2、创建请求
-//'https://movie.douban.com/top250'
+
 //'https://usafacts.org/visualizations/coronavirus-covid-19-spread-map'
 //https://www.corona.help/country/united-states
 https.get('https://www.worldometers.info/coronavirus/country/us/',
@@ -23,53 +25,73 @@ function(res){
 		//console.log(html);  //yes
 		//let htmlStr=Buffer.concat(html).toString('utf-8');
 		let $=cheerio.load(html);
-		//console.log($('.container:first').html());
-		//console.log($('.container .row .col-md-8 .tab-content .tab-pane .usa_table_countries_div .table .total_row_usa odd>td'));
 		//console.log($.html()); //yes
-		//.tab-pane .usa_table_countries_div .table .even .mt_a
-		//.attr('href')
-		//console.log($('.tab-content .total_row_usa').html());
 		//console.log($('.tab-content .total_row_usa').find('td').eq(2).html()); //yes
 		let usaTotal=$('.tab-content .total_row_usa').find('td').eq(2).html();
 		let usaDeath=$('.tab-content .total_row_usa').find('td').eq(4).html();
 		console.log(usaTotal,usaDeath);
 		console.log(typeof usaDeath);
 		console.log("*************");
-		let tmpStateName=null;
-		let tmpTotalCases=null;
-		let tmpTotalDeath=null;
-		let tmpJson={
-			name:null,
-			totalCases:null,
-			totalDeath:null
-		}
 		let i=0;
 		$('.tab-content').find('tbody').find('tr').each(function(){
+			let tmpJson={
+				name:null,
+				totalCases:null,
+				totalDeath:null
+			}
 			if(i===0){
 				tmpJson.name="USA Total";
 				
 			}else{
 				tmpJson.name=$('.mt_a',this).html();
 			}
-			tmpTotalCases=($('td',this).eq(2).html()).replace(/\,/g,"");
+			
+			let tmpTotalCases=($('td',this).eq(2).html()).replace(/\,/g,"");
 			tmpJson.totalCases=Number(tmpTotalCases);
-			tmpTotalDeath=($('td',this).eq(4).html()).replace(/\,/g,"");
+			let tmpTotalDeath=($('td',this).eq(4).html()).replace(/\,/g,"");
 			tmpTotalDeath=tmpTotalDeath.replace('\n','');
 			tmpJson.totalDeath=Number(tmpTotalDeath);
 			
-			console.log(tmpJson);
+			//console.log(tmpJson);
 			allData.push(tmpJson);
 			//console.log(allData[i].name+":"+allData[i].totalCases+allData[i].totalDeath);
 			i++;
-			/*
-			console.log($('.mt_a',this).html());
-			console.log($('td',this).eq(2).html());
-			console.log($('td',this).eq(4).html());
-			*/
 		})
 		
+		//51个州+1个特别行政区+一个USA totalNum
+		const statesNum=53;
+		for(i=0;i<statesNum;i++){
+			data51.push(allData[i]);		
+		}
+		for(i=0;i<statesNum;i++){
+			if(data51[i].name==null){
+				let tmp=data51[52];
+				tmp.name="Puerto Rico";
+				data51[52]=data51[i];
+				data51[i]=tmp;
+			}
+		}
+		
+		for(i=1;i<statesNum-1;i++){
+			for(let j=1;j<statesNum-1;j++){
+				if(data51[i].name<data51[j].name){
+					let tmp=data51[i];
+					data51[i]=data51[j];
+					data51[j]=tmp;
+				}
+			}
+		}
+		/*
+		for(i=0;i<statesNum;i++){
+			console.log(data51[i]);
+		}
+		*/
+		fs.writeFile('../USA/static/json/usaCases.js',JSON.stringify(data51),function(err){
+			if(!err){
+				console.log('文件写入完毕！');
+			}
+		})
 	})
 })
 
-//console.log(allData);
 
